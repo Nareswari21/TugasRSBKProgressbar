7c0381cb276f2e1495fc0e8b25b08af421988517
Nareswari21 <nareswari219@gmail.com>
2020-05-15 22:28:03 +0700

    first

---
+++ UseComponent/src/CounterApples.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.event.ActionEvent;
+import javax.swing.JApplet;
+import javax.swing.JProgressBar;
+
+/**
+ *
+ * @author ACER
+ */
+public class CounterApples extends JApplet {
+
+    JProgressBar jProgressBar1;
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */  
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+
+    // TODO overwrite start(), stop() and destroy() methods
+
+    void jButton1ActionPerformed2(ActionEvent evt) {
+    }
+
+    void jButton1ActionPerformed3(ActionEvent evt) {
+    }
+}
